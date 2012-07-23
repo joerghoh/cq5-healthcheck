68b16ab33c6f8438a71f1ac59c201e3aabf5d5f4
joerghoh <joerg@joerghoh.de>
2012-07-23 09:31:49 +0200

    expected number of active bundles can be configured via OSGI

--- core/src/main/java/de/joerghoh/cq5/healthcheck/providers/BundleStatusProvider.java
+++ core/src/main/java/de/joerghoh/cq5/healthcheck/providers/BundleStatusProvider.java
@@ -4,10 +4,13 @@ import java.util.Dictionary;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.sling.commons.osgi.OsgiUtil;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.ComponentContext;
+
 
 import de.joerghoh.cq5.healthcheck.HealthStatus;
 import de.joerghoh.cq5.healthcheck.HealthStatusProvider;
@@ -23,30 +26,40 @@ import de.joerghoh.cq5.healthcheck.HealthStatusProvider;
 public class BundleStatusProvider implements HealthStatusProvider {
 
 	
+	private static final int DEFAULT_NO_ACTIVE_BUNDLES = 238;
+	@Property (intValue=DEFAULT_NO_ACTIVE_BUNDLES)
+	private static final String NO_ACTIVE_BUNDLES = "active.bundles";
+	
+	private int activeBundlesMatch;
+	
 	private BundleContext bctx;
 	
 	public HealthStatus getHealthStatus() {
 		 Bundle[] bundles = bctx.getBundles();
-		 int notActiveBundleCount = 0;
+		 int activeBundleCount = 0;
 		 for (Bundle b: bundles) {
 			
 			 Dictionary<String,String> headers = b.getHeaders();
 			 boolean isFragment = (headers.get("Fragment-Host") != null);
 			 boolean isActive = (b.getState() == Bundle.ACTIVE);
 			 
-			 if (!isFragment && !isActive) {
-				 notActiveBundleCount++;
-			 }
 			 
+			 if (!isFragment && isActive) {
+				 activeBundleCount++;
+			 }
 			 
 		 }
 		 
-		 String statusMessage = notActiveBundleCount + " bundles not active";
-		 int status = HS_WARN;
-		 if (notActiveBundleCount == 0) {
+		 String statusMessage;
+		 int status;
+		 if (activeBundleCount == activeBundlesMatch) {
 			 status = HS_OK;
 			 statusMessage = "all bundles active";
+		 } else {
+			 status = HS_WARN;
+			 statusMessage = activeBundleCount + " bundles active ("+activeBundlesMatch + " expected)";
 		 }
+		 
 		 String providerName = this.getClass().getName();
 		 return new HealthStatus(status,statusMessage,providerName);
 		 
@@ -55,6 +68,7 @@ public class BundleStatusProvider implements HealthStatusProvider {
 	@Activate
 	protected void activate (ComponentContext ctx) {
 		bctx = ctx.getBundleContext();
+		activeBundlesMatch = OsgiUtil.toInteger (ctx.getProperties().get(NO_ACTIVE_BUNDLES),DEFAULT_NO_ACTIVE_BUNDLES);
 	}
 	
 }
