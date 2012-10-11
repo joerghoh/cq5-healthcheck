87719189256ec8799eeae6981bf106d3f2d7dc01
joerghoh <joerg@joerghoh.de>
2012-10-11 21:52:50 +0200

    we don't need these any more, we will directly go for their JMX representations

--- core/src/main/java/de/joerghoh/cq5/healthcheck/jmx/RequestCounterMBean.java
+++
@@ -1,6 +0,0 @@
-package de.joerghoh.cq5.healthcheck.jmx;
-
-public interface RequestCounterMBean {
-
-	public int getRequestCount();
-}
--- core/src/main/java/de/joerghoh/cq5/healthcheck/providers/replication/ReplicationAgentStatusUtil.java
+++
@@ -1,8 +0,0 @@
-package de.joerghoh.cq5.healthcheck.providers.replication;
-
-public class ReplicationAgentStatusUtil {
-
-	public static final String QUEUE_ERROR_LENGTH = "queue.error.length";
-	public static final String QUEUE_WARN_LENGTH = "queue.warn.length";
-	
-}
