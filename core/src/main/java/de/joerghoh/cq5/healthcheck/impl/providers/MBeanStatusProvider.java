ba86fd956393576300b41be7cd6a28cf086c1ece
joerghoh <joerg@joerghoh.de>
2012-11-16 00:47:39 +0100

    Update license headers
    Change all remaining direct copies of the license headers to the correct
    headers. No all java files should have the correct one

--- core/src/main/java/de/joerghoh/cq5/healthcheck/impl/providers/MBeanStatusProvider.java
+++ core/src/main/java/de/joerghoh/cq5/healthcheck/impl/providers/MBeanStatusProvider.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2012 Jörg Hoh, Alexander Saar, Markus Haack
  * 
- *   Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
