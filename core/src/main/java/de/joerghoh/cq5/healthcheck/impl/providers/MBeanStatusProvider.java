e8b030c1bd4a274e8eaf4185305a4f0a4002a207
Alexander Saar <alexander.saar@gmail.com>
2013-04-05 10:40:28 +0200

    issue #36 - fix dependencies so that build works again

--- core/src/main/java/de/joerghoh/cq5/healthcheck/impl/providers/MBeanStatusProvider.java
+++ core/src/main/java/de/joerghoh/cq5/healthcheck/impl/providers/MBeanStatusProvider.java
@@ -20,13 +20,9 @@ import java.util.Dictionary;
 import java.util.Set;
 
 import javax.jcr.RepositoryException;
-import javax.management.AttributeNotFoundException;
-import javax.management.InstanceNotFoundException;
-import javax.management.MBeanException;
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
-import javax.management.ReflectionException;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.math.NumberUtils;
@@ -45,31 +41,28 @@ import de.joerghoh.cq5.healthcheck.Status;
 import de.joerghoh.cq5.healthcheck.StatusCode;
 import de.joerghoh.cq5.healthcheck.StatusProvider;
 
-
 @Service
-@Component(label = "MBean Status Provider Factory", metatype = true, 
-	configurationFactory = true, policy = ConfigurationPolicy.REQUIRE)
-
+@Component(label = "MBean Status Provider Factory", metatype = true, configurationFactory = true, policy = ConfigurationPolicy.REQUIRE)
 public class MBeanStatusProvider implements StatusProvider {
 
-
-	private static final Logger log = LoggerFactory.getLogger(MBeanStatusProvider.class);
+	private static final Logger log = LoggerFactory
+			.getLogger(MBeanStatusProvider.class);
 
 	private MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-	
+
 	@Property
 	private static String MBEAN_NAME = "mbean.name";
 	private String mbeanName;
-	
+
 	@Property
 	private static String MBEAN_PROPERTY = "mbean.property";
 	private String[] properties;
-	
+
 	@Property
 	private static String MBEAN_PROVIDER_HINT = "mbean.providerHint";
 	private String providerHint;
 	private String providerName;
-	
+
 	private ObjectName mbean;
 	private String statusMessage = "";
 
@@ -83,24 +76,25 @@ public class MBeanStatusProvider implements StatusProvider {
 			return new Status(status, statusMessage, providerName);
 		} else {
 			log.info("Cannot resolve mbean " + mbeanName);
-			return new Status(StatusCode.UNKNOWN,"cannot find mbean", providerName);
+			return new Status(StatusCode.UNKNOWN, "cannot find mbean",
+					providerName);
 		}
 	}
 
 	@Activate
 	protected void activate(ComponentContext ctx) throws RepositoryException {
 
-		Dictionary<?,?> props = ctx.getProperties();
+		Dictionary<?, ?> props = ctx.getProperties();
 		mbeanName = PropertiesUtil.toString(props.get(MBEAN_NAME), null);
 		properties = PropertiesUtil.toStringArray(props.get(MBEAN_PROPERTY));
-		providerHint = PropertiesUtil.toString(props.get(MBEAN_PROVIDER_HINT), null);
-
+		providerHint = PropertiesUtil.toString(props.get(MBEAN_PROVIDER_HINT),
+				null);
 
 		mbean = buildObjectName(mbeanName);
 		if (mbean != null && mbeanExists(mbean)) {
-			log.info("Instantiate healtcheck for MBean {}",mbeanName);
+			log.info("Instantiate healtcheck for MBean {}", mbeanName);
 		} else {
-			log.warn ("Cannot instantiate healthcheck for MBean {}", mbeanName);
+			log.warn("Cannot instantiate healthcheck for MBean {}", mbeanName);
 		}
 		providerName = mbeanName;
 		if (providerHint != null) {
@@ -123,26 +117,30 @@ public class MBeanStatusProvider implements StatusProvider {
 		StatusCode accumulatedStatus = StatusCode.OK;
 
 		statusMessage = "";
-		
+
 		for (String property : properties) {
-			
+
 			/*
-			 * value might be: "my.JMX.propertyName.warn.>", and we need the triple of "jmxAttributeName","level" and "type of comparison".
-			 * The jmxAttributeName might contain dots, so we need to reverse to use String.split()
+			 * value might be: "my.JMX.propertyName.warn.>", and we need the
+			 * triple of "jmxAttributeName","level" and "type of comparison".
+			 * The jmxAttributeName might contain dots, so we need to reverse to
+			 * use String.split()
 			 */
 
 			final String key = new StringBuffer(property).reverse().toString(); // revert
-																					// it
+																				// it
 			final String[] split = key.split("\\.", 4);
 			if (split.length != 4) {
 				// key does not match the needed configuration triple
 				continue;
 			}
-			final String comparisonAttributeName = new StringBuffer(split[3]).reverse().toString();
-			
+			final String comparisonAttributeName = new StringBuffer(split[3])
+					.reverse().toString();
+
 			// what would be the statusCode if we have a match?
 
-			final String comparisonLevel = new StringBuffer(split[2]).reverse().toString();
+			final String comparisonLevel = new StringBuffer(split[2]).reverse()
+					.toString();
 			StatusCode statusCode = StatusCode.OK;
 
 			if (comparisonLevel.equalsIgnoreCase("warn")) {
@@ -153,27 +151,32 @@ public class MBeanStatusProvider implements StatusProvider {
 				log.warn("Ignoring property (invalid level): " + property);
 				continue;
 			}
-			
-			final String comparisonOperation = new StringBuffer(split[1]).reverse().toString();
+
+			final String comparisonOperation = new StringBuffer(split[1])
+					.reverse().toString();
 
 			// retrieve the long value for comparison
-			final String comparisonValue = new StringBuffer(split[0]).reverse().toString();
+			final String comparisonValue = new StringBuffer(split[0]).reverse()
+					.toString();
 			log.debug("compare {} and {}", property, comparisonValue);
 
 			// read the correct value via JMX
 			Object jmxValueObj = getAttributeValue(comparisonAttributeName);
 			if (jmxValueObj == null) {
 
-				log.warn("Ignoring property " + property + " (no such JMX attribute " + comparisonAttributeName + ")");
-				statusMessage ="Cannot resolve property or MBean";
+				log.warn("Ignoring property " + property
+						+ " (no such JMX attribute " + comparisonAttributeName
+						+ ")");
+				statusMessage = "Cannot resolve property or MBean";
 				return StatusCode.WARN;
 			}
 			log.debug("jmx value = {}", jmxValueObj);
 
 			// do the comparison
 			boolean match = false;
-			try  {
-				match = compareAttributeValue(comparisonOperation, comparisonValue, jmxValueObj);
+			try {
+				match = compareAttributeValue(comparisonOperation,
+						comparisonValue, jmxValueObj);
 			} catch (RuntimeException e) {
 				log.info("Ignoring property (invalid value type): " + property);
 				continue;
@@ -184,7 +187,10 @@ public class MBeanStatusProvider implements StatusProvider {
 				if (statusMessage.length() > 0) {
 					statusMessage += ", ";
 				}
-				statusMessage += comparisonAttributeName + " = " + (jmxValueObj.getClass().isArray() ? ArrayUtils.toString(jmxValueObj) : jmxValueObj.toString());
+				statusMessage += comparisonAttributeName
+						+ " = "
+						+ (jmxValueObj.getClass().isArray() ? ArrayUtils
+								.toString(jmxValueObj) : jmxValueObj.toString());
 			}
 
 			// if we have a match, update the overall status
@@ -205,33 +211,30 @@ public class MBeanStatusProvider implements StatusProvider {
 		try {
 			return server.getAttribute(mbean, attributeName);
 		} catch (Exception e) {
-			log.warn("Cannot read attribute "+attributeName+" from MBean "+mbean);
-		} 
+			log.warn("Cannot read attribute " + attributeName + " from MBean "
+					+ mbean);
+		}
 		return null;
 	}
 
 	/**
 	 * compare helper method to compare the different types of attribute values
-	 * supported types:
-	 * ** long
-	 * ** integer
-	 * ** boolean
-	 * ** string
+	 * supported types: ** long ** integer ** boolean ** string
 	 * 
 	 * @param comparisonOperation
 	 * @param comparisonValue
 	 * @param jmxValueObj
 	 * @return
 	 */
-	private boolean compareAttributeValue(final String comparisonOperation, final String comparisonValue, Object jmxValueObj) {
-		
+	private boolean compareAttributeValue(final String comparisonOperation,
+			final String comparisonValue, Object jmxValueObj) {
+
 		boolean match = false;
-		
-		
-		
+
 		if (jmxValueObj instanceof Long) { // first check for plain value
 			final long jmxLongValue = Long.parseLong(jmxValueObj.toString());
-			final long comparisonLongValue = NumberUtils.createLong(comparisonValue);
+			final long comparisonLongValue = NumberUtils
+					.createLong(comparisonValue);
 			if (comparisonOperation.equals(">")) {
 				match = (jmxLongValue > comparisonLongValue);
 			} else if (comparisonOperation.equals("==")) {
@@ -241,13 +244,15 @@ public class MBeanStatusProvider implements StatusProvider {
 			} else if (comparisonOperation.equals("<")) {
 				match = (jmxLongValue < comparisonLongValue);
 			} else {
-				log.warn("Can not compare long values {} and {}", jmxLongValue, comparisonLongValue);
+				log.warn("Can not compare long values {} and {}", jmxLongValue,
+						comparisonLongValue);
 				throw new RuntimeException();
 			}
-			
-		} else if (jmxValueObj instanceof Integer) { 
+
+		} else if (jmxValueObj instanceof Integer) {
 			final int jmxIntValue = Integer.parseInt(jmxValueObj.toString());
-			final int comparisonIntValue = NumberUtils.createInteger(comparisonValue);
+			final int comparisonIntValue = NumberUtils
+					.createInteger(comparisonValue);
 			if (comparisonOperation.equals(">")) {
 				match = (jmxIntValue > comparisonIntValue);
 			} else if (comparisonOperation.equals("==")) {
@@ -257,25 +262,31 @@ public class MBeanStatusProvider implements StatusProvider {
 			} else if (comparisonOperation.equals("<")) {
 				match = (jmxIntValue < comparisonIntValue);
 			} else {
-				log.warn("Can not compare int values {} and {}", jmxIntValue, comparisonIntValue);
+				log.warn("Can not compare int values {} and {}", jmxIntValue,
+						comparisonIntValue);
 				throw new RuntimeException();
 			}
-			
+
 		} else if (jmxValueObj instanceof long[]) { // second check for array
 			final long[] jmxLongArrayValue = (long[]) jmxValueObj;
-			final long comparisonLongValue = NumberUtils.createLong(comparisonValue);
+			final long comparisonLongValue = NumberUtils
+					.createLong(comparisonValue);
 			if (comparisonOperation.equals(">")) {
 				match = (NumberUtils.max(jmxLongArrayValue) > comparisonLongValue);
 			} else if (comparisonOperation.equals("==")) {
-				// TODO maybe it makes more sense for == to compare all array values?
-				match = ArrayUtils.contains(jmxLongArrayValue, comparisonLongValue);
+				// TODO maybe it makes more sense for == to compare all array
+				// values?
+				match = ArrayUtils.contains(jmxLongArrayValue,
+						comparisonLongValue);
 			} else if (comparisonOperation.equals("<")) {
 				match = (NumberUtils.min(jmxLongArrayValue) < comparisonLongValue);
 			} else {
-				log.warn("Can not compare long array values {} and {}", jmxLongArrayValue, comparisonLongValue);
+				log.warn("Can not compare long array values {} and {}",
+						jmxLongArrayValue, comparisonLongValue);
 				throw new RuntimeException();
 			}
-		} else if (jmxValueObj instanceof String) { // third check for String values
+		} else if (jmxValueObj instanceof String) { // third check for String
+													// values
 			log.info("type comparison: string");
 			final String jmxStringValue = jmxValueObj.toString();
 			if (comparisonOperation.equals("equals")) {
@@ -283,12 +294,14 @@ public class MBeanStatusProvider implements StatusProvider {
 			} else if (comparisonOperation.equals("notequals")) {
 				match = (!comparisonValue.equals(jmxStringValue));
 			} else {
-				log.warn("Can not compare String values {} and {}", jmxStringValue, comparisonValue);
+				log.warn("Can not compare String values {} and {}",
+						jmxStringValue, comparisonValue);
 				throw new RuntimeException();
 			}
 		} else if (jmxValueObj instanceof Boolean) {
 			final boolean jmxBooleanValue = (Boolean) jmxValueObj;
-			final boolean comparisonBooleanValue = Boolean.parseBoolean(comparisonValue);
+			final boolean comparisonBooleanValue = Boolean
+					.parseBoolean(comparisonValue);
 			if (comparisonOperation.equals("equals")) {
 				match = (comparisonBooleanValue == jmxBooleanValue);
 			} else if (comparisonOperation.equals("==")) {
@@ -296,17 +309,19 @@ public class MBeanStatusProvider implements StatusProvider {
 			} else if (comparisonOperation.equals("notequals")) {
 				match = (comparisonBooleanValue != jmxBooleanValue);
 			} else {
-				log.warn("Can not compare boolean values {} and {}", jmxBooleanValue, comparisonBooleanValue);
+				log.warn("Can not compare boolean values {} and {}",
+						jmxBooleanValue, comparisonBooleanValue);
 				throw new RuntimeException();
 			}
 		} else {
-			log.warn("Can not compare jmx attribute value {} with {}", jmxValueObj, comparisonValue);
+			log.warn("Can not compare jmx attribute value {} with {}",
+					jmxValueObj, comparisonValue);
 			log.warn("jmxValueObj type = " + jmxValueObj.getClass().getName());
 			throw new RuntimeException();
 		}
 		return match;
 	}
-	
+
 	private ObjectName buildObjectName(String name) {
 		ObjectName mbean = null;
 		try {
